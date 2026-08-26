ead6812ea8e2392b659381534cdf3d35227bff40
Delevone <grumpysucks@gmail.com>
2026-08-25 21:39:11 -0500

    Removed comments from init.py

---
+++ init.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class MyBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print(f"We are ready to go in, {self.user.name}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if "shit" in message.content.lower():
+            await message.delete()
+            await message.channel.send(f"{message.author.mention} - That word is prohibited!")
+        else:
+            print(f"Message from {message.author}: {message.content}")
+
+        await self.process_commands(message)
